0f4e23de1db4d29975b44c9149ebe3abd0e24dee
Dmytro Lobanov <dspwww@gmail.com>
2025-04-05 15:17:27 +0100

    featL Add operate function

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 console.log("Welcome!")
 
+let firstNumber;
+let secondNumber;
+let operator;
+
 function add(a, b) {
   return a + b;
 }
@@ -14,4 +18,24 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+function operate(firstNumber, operator, secondNumber) {
+  switch(operator) {
+    case "+": 
+      add(firstNumber, operator, secondNumber);
+      break;
+    case "-":
+      subtract(firstNumber, operator, secondNumber);
+      break;
+    case "*":
+      multiply(firstNumber, operator, secondNumber);
+      break;
+    case "/": 
+      divide(firstNumber, operator, secondNumber);
+      break;
+    default:
+      console.error('Unknown operator', operator);
+      break;
+  }
 }
