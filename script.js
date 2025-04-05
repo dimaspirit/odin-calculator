ea8a4aa973fa49cbe40e23464c36f05bffdfce8c
Dmytro Lobanov <dspwww@gmail.com>
2025-04-05 15:07:36 +0100

    feat: Add 4 functions: +, -, *, /

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Welcome!")
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
